c320f70226f7f932cc1b45ca6b2cfa63b63a5472
unknown <melena.art@gmail.com>
2013-10-03 00:11:00 +0400

    initial solution commit

---
+++ University/DAL/SchoolContext.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using University.Models;
+
+namespace University.DAL
+{
+    public class SchoolContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>(); //prevents table names from being pluralized
+        }
+    }
+}
