7a35d8fc81f16ae924e50d158e7c77cc9a3d3997
unknown <melena.art@gmail.com>
2013-10-12 20:38:02 +0400

    removed unused using

--- University/Controllers/DepartmentController.cs
+++ University/Controllers/DepartmentController.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using University.Models;
 using University.DAL;
--- University/Controllers/InstructorController.cs
+++ University/Controllers/InstructorController.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using University.Models;
 using University.DAL;
